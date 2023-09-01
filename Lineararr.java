124643ddcaae51a0a82addadabd0fb0a88734a4c
KashypHi <himanshu.ak205@gmail.com>
2023-09-01 12:36:56 +0530

    error solve

--- Lineararr.java
+++ Lineararr.java
@@ -4,10 +4,12 @@ public class Lineararr {
        String tar="watermelon";
        boolean flag = true; 
        for(int i=0;i<fruits.length; i++){
-        if(fruits[i].equals(tar))
+        if(fruits[i].equals(tar)){
+
+            System.out.println(tar+ "Is present ");
+            break;
+        }
        
-        System.out.println(tar+ "Is present ");
-        break;
        }
     
     if(flag ==false);{
